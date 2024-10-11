cbd80fed2a8e6d2d5f0d7ddefc119a62643c6ec9
first commit <joselinalfaro71@gmail.com>
2024-10-11 16:25:18 -0500

    avances en el inicio

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,16 @@
+import SideBar from '@/app/components/SideBar'
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Adrian', lastName: 'JSM' }
   return (
-    <main>
-        SIDEBAR
-        {children}
+
+    <main className='flex h-screen w-full font-inter' >
+      <SideBar user={loggedIn} />
+      {children}
     </main>
-);
+  );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,34 @@
+import HeaderBox from '@/app/components/HeaderBox'
 import React from 'react'
+import TotalBalanceBox from '../components/TotalBalanceBox';
 
 const Home = () => {
+
+  const loggedIn = {
+    firstName: "Joselin"
+  };
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="Bienvenido"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Acces  and manage your account and transaction efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.53}
+
+          />
+        </header>
+
+
+
+      </div>
+    </section>
   )
 }
 
